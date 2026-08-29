0a4c1b630974ec2a466279b73ba76094bd13e339
Guilherme <buasquevicz22@outlook.com>
2026-08-29 12:07:02 -0300

    test: adiciona geração de relatório Mochawesome

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from "cypress"
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-
   e2e: {
-    baseUrl : "https://www.saucedemo.com/",
+    baseUrl: "https://www.saucedemo.com/",
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports/mocha",
+      overwrite: false,
+      html: false,
+      json: true,
+    },
     setupNodeEvents(on, config) {
-      return config
+      return config;
     },
   },
 });
